860cd30020ccfedad606c863648aafe06161b48d
fazza7777 <aungbarlaay7777@gmail.com>
2022-11-06 00:00:21 +0630

    'use_intl_package'

--- lib/screen/home_screen/home_screen.dart
+++ lib/screen/home_screen/home_screen.dart
@@ -24,6 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white70.withOpacity(.7),
       appBar: AppBar(
         title: const Text('Covid Tracker'),
         centerTitle: true,
--- lib/widget/home_main_widget.dart
+++ lib/widget/home_main_widget.dart
@@ -1,5 +1,6 @@
 import 'package:covid_tracker/model/summary_ob.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 Widget homeMainWidget(SummaryOb summaryOb) {
   var global = summaryOb.global!;
@@ -9,121 +10,135 @@ Widget homeMainWidget(SummaryOb summaryOb) {
       Card(
         child: Padding(
           padding: const EdgeInsets.all(10.0),
-          child: Row(
+          child: Column(
             children: [
-              // Total Confirmed
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('Total Confirmed',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16)),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "${global.totalConfirmed}",
-                      style: const TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    RichText(
-                      text: TextSpan(
-                          text: "New : ",
-                          style: const TextStyle(color: Colors.black54),
-                          children: [
-                            TextSpan(
-                                text: "${global.newConfirmed}",
-                                style: const TextStyle(
-                                    color: Colors.blue,
-                                    fontWeight: FontWeight.bold))
-                          ]),
-                    )
-                  ],
-                ),
+              Text(
+                'Update date : ${DateFormat("EEEE, MMMM dd, yyyy, hh : MM").format(
+                  DateTime.parse(global.date!),
+                )}',
+                style: const TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 13),
               ),
-              // Total Death
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('Total Deaths',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16)),
-                    const SizedBox(
-                      height: 10,
+              const SizedBox(height: 10,),
+              Row(
+                children: [
+                  // Total Confirmed
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Total\nConfirmed',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16)),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "${global.totalConfirmed}",
+                          style: const TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        RichText(
+                          text: TextSpan(
+                              text: "New : ",
+                              style: const TextStyle(color: Colors.black54),
+                              children: [
+                                TextSpan(
+                                    text: "${global.newConfirmed}",
+                                    style: const TextStyle(
+                                        color: Colors.blue,
+                                        fontWeight: FontWeight.bold))
+                              ]),
+                        )
+                      ],
                     ),
-                    Text(
-                      "${global.totalDeaths}",
-                      style: const TextStyle(
-                          color: Colors.red,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
+                  ),
+                  // Total Death
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Total\nDeaths',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16)),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "${global.totalDeaths}",
+                          style: const TextStyle(
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        RichText(
+                          text: TextSpan(
+                              text: "New : ",
+                              style: const TextStyle(color: Colors.black54),
+                              children: [
+                                TextSpan(
+                                    text: "${global.newDeaths}",
+                                    style: const TextStyle(
+                                        color: Colors.red,
+                                        fontWeight: FontWeight.bold))
+                              ]),
+                        )
+                      ],
                     ),
-                    const SizedBox(
-                      height: 5,
+                  ),
+                  // Total Recover
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Total\nRecovered',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16)),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "${global.totalRecovered}",
+                          style: const TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        RichText(
+                          text: TextSpan(
+                              text: "New : ",
+                              style: const TextStyle(color: Colors.black54),
+                              children: [
+                                TextSpan(
+                                    text: "${global.newRecovered}",
+                                    style: const TextStyle(
+                                        color: Colors.green,
+                                        fontWeight: FontWeight.bold))
+                              ]),
+                        )
+                      ],
                     ),
-                    RichText(
-                      text: TextSpan(
-                          text: "New : ",
-                          style: const TextStyle(color: Colors.black54),
-                          children: [
-                            TextSpan(
-                                text: "${global.newDeaths}",
-                                style: const TextStyle(
-                                    color: Colors.red,
-                                    fontWeight: FontWeight.bold))
-                          ]),
-                    )
-                  ],
-                ),
-              ),
-              // Total Recover
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('Total Recovered',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16)),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "${global.totalRecovered}",
-                      style: const TextStyle(
-                          color: Colors.green,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    RichText(
-                      text: TextSpan(
-                          text: "New : ",
-                          style: const TextStyle(color: Colors.black54),
-                          children: [
-                            TextSpan(
-                                text: "${global.newRecovered}",
-                                style: const TextStyle(
-                                    color: Colors.green,
-                                    fontWeight: FontWeight.bold))
-                          ]),
-                    )
-                  ],
-                ),
+                  ),
+                ],
               ),
             ],
           ),
